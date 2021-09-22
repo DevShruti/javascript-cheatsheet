ce7d96c1c46e0e42c7895d53bfd453384687441b
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-09-22 11:59:54 -0500

    java in html page

---
+++ javasrciptBasics.js
@@ -0,0 +1,9 @@
+//Including JavaScript in an HTML Page
+//To include JavaScript inside a page, you need to wrap it in <script> tags:
+
+<script type="text/javascript">
+
+//JS code goes here
+
+</script>
+
